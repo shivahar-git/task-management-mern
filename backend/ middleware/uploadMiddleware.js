ec821e91d95115e849a4ab784e113ea79ab55381
shivahar <168921705+shivahar-git@users.noreply.github.com>
2026-08-21 23:16:08 +0530

    Update uploadMiddleware.js

--- backend/ middleware/uploadMiddleware.js
+++ backend/ middleware/uploadMiddleware.js
@@ -1,1 +1,31 @@
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+
+  params: {
+    folder: "task-management",
+
+    allowed_formats: [
+      "jpg",
+      "jpeg",
+      "png",
+      "pdf",
+      "doc",
+      "docx",
+    ],
+
+    resource_type: "auto",
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+});
+
+module.exports = upload;
