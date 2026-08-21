a3cbe60537a952f53fdd7fda89df812449a8c129
shivahar <168921705+shivahar-git@users.noreply.github.com>
2026-08-21 23:16:25 +0530

    Update errorMiddleware.js

--- backend/ middleware/errorMiddleware.js
+++ backend/ middleware/errorMiddleware.js
@@ -1,1 +1,23 @@
+const notFound = (req, res) => {
+  res.status(404).json({
+    message: `Route not found: ${req.originalUrl}`,
+  });
+};
 
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  const statusCode =
+    res.statusCode === 200
+      ? 500
+      : res.statusCode;
+
+  res.status(statusCode).json({
+    message: err.message || "Server Error",
+  });
+};
+
+module.exports = {
+  notFound,
+  errorHandler,
+};
