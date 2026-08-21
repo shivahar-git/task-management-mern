3361d3b2a0297fec4179915f79d0ffbfb05cc612
shivahar <168921705+shivahar-git@users.noreply.github.com>
2026-08-21 23:22:23 +0530

    Update AuthContext.jsx

--- frontend/src/ context/AuthContext.jsx
+++ frontend/src/ context/AuthContext.jsx
@@ -1,1 +1,92 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
+import API from "../services/api";
+
+
+const AuthContext =
+  createContext();
+
+
+export const AuthProvider = ({
+  children,
+}) => {
+  const [user, setUser] =
+    useState(null);
+
+  const [loading, setLoading] =
+    useState(true);
+
+
+  useEffect(() => {
+    const storedUser =
+      localStorage.getItem("user");
+
+    const token =
+      localStorage.getItem("token");
+
+    if (storedUser && token) {
+      setUser(
+        JSON.parse(storedUser)
+      );
+    }
+
+    setLoading(false);
+  }, []);
+
+
+  const login = (
+    userData,
+    token
+  ) => {
+    localStorage.setItem(
+      "token",
+      token
+    );
+
+    localStorage.setItem(
+      "user",
+      JSON.stringify(userData)
+    );
+
+    setUser(userData);
+  };
+
+
+  const logout = () => {
+    localStorage.removeItem(
+      "token"
+    );
+
+    localStorage.removeItem(
+      "user"
+    );
+
+    setUser(null);
+  };
+
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+
+export const useAuth = () => {
+  return useContext(
+    AuthContext
+  );
+};
