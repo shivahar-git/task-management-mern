a888015648394860986e0524cf270572b3a81549
shivahar <168921705+shivahar-git@users.noreply.github.com>
2026-08-21 23:19:32 +0530

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,1 +1,51 @@
+const express = require("express");
 
+const router = express.Router();
+
+const protect = require(
+  "../middleware/authMiddleware"
+);
+
+const upload = require(
+  "../middleware/uploadMiddleware"
+);
+
+const {
+  getTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require(
+  "../controllers/taskController"
+);
+
+
+router
+  .route("/")
+  .get(protect, getTasks)
+  .post(
+    protect,
+    upload.single("file"),
+    createTask
+  );
+
+
+router
+  .route("/:id")
+  .get(
+    protect,
+    getTaskById
+  )
+  .put(
+    protect,
+    upload.single("file"),
+    updateTask
+  )
+  .delete(
+    protect,
+    deleteTask
+  );
+
+
+module.exports = router;
